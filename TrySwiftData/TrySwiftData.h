c7c77555db64e7ed5d7ef12641c2354b4d084ed2
NatashaTheRobot <natasha@natashatherobot.com>
2016-12-26 14:30:26 -0300

    Initial Commit

---
+++ TrySwiftData/TrySwiftData.h
@@ -0,0 +1,19 @@
+//
+//  TrySwiftData.h
+//  TrySwiftData
+//
+//  Created by Natasha Murashev on 12/26/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrySwiftData.
+FOUNDATION_EXPORT double TrySwiftDataVersionNumber;
+
+//! Project version string for TrySwiftData.
+FOUNDATION_EXPORT const unsigned char TrySwiftDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrySwiftData/PublicHeader.h>
+
+
